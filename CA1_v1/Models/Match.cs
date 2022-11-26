cb257b0374d4656c7a53999eb2093c8967097b84
dacillo <johncarlodacillo123@gmail.com>
2022-11-26 15:17:10 +0000

    added model

---
+++ CA1_v1/Models/Match.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CA1_v1.Models
+{
+    public class Match
+    {
+        public int MatchID { get; set; }
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string? Venue { get; set; }
+
+        [StringLength(100, MinimumLength = 2)]
+        public string? Opponent { get; set; }
+
+        [Range(0, 100)]
+        public int? GoalsFor { get; set; }
+
+        [Range(0, 100)]
+        public int? GoalsAgainst { get; set; }
+
+    }
+}
